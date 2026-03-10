61b1630939aa210efb7df4a719835da41b7f6d47
Saad Belabbes <sbelabbe@c2r2p1.1337.ma>
2026-03-10 22:25:27 +0000

    P00

--- ex5/ft_count_harvest_recursive.py
+++
@@ -1,9 +0,0 @@
-def ft_count_harvest_recursive(day=1):
-    if day == 1:
-        ft_count_harvest_recursive.n = int(input("Days until harvest: "))
-    if day > ft_count_harvest_recursive.n:
-        print("Harvest time!")
-        return
-    print("day", day)
-    ft_count_harvest_recursive(day + 1)
-ft_count_harvest_recursive()
